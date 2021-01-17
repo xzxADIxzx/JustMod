649bd2eae630268c83bf5e50b374fe619f69cda9
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-01-17 10:51:22 +0300

    ...

--- scripts/GenericSmelter/cotton-wool-mixer.js
+++ scripts/GenericSmelter/cotton-wool-mixer.js
@@ -1,8 +1,8 @@
 var speed = 4.2;
 	const cottonWoolMixer = extendContent(GenericCrafter, "cotton-wool-mixer", {
-		Draw.drawLayer(tile){
-		Draw.rect(this.name + "-rotator", Draw.drawx(), Draw.drawy(), this.totalProgress * speed);
-		Draw.rect(this.name + "-top", Draw.drawx(), Draw.drawy(), 0);
+		drawLayer(tile){
+		Draw.rect(this.name + "-rotator", drawx(), drawy(), this.totalProgress * speed);
+		Draw.rect(this.name + "-top", drawx(), drawy(), 0);
 		}
 	});
 	cottonWoolMixer.layer = Layer.turret;
--- scripts/GenericSmelter/silicone-mixer.js
+++ scripts/GenericSmelter/silicone-mixer.js
@@ -1,8 +1,8 @@
 var speed = -0.5;
 	const siliconeMixer = extendContent(GenericCrafter, "silicone-mixer", {
-		Draw.drawLayer(tile){
-		Draw.rect(this.name + "-rotator", Draw.drawx(), Draw.drawy(), this.totalProgress * speed);
-		Draw.rect(this.name + "-top", Draw.drawx(), Draw.drawy(), 0);
+		drawLayer(tile){
+		Draw.rect(this.name + "-rotator", drawx(), drawy(), this.totalProgress * speed);
+		Draw.rect(this.name + "-top", drawx(), drawy(), 0);
 		}
 	});
 	siliconeMixer.layer = Layer.turret;
