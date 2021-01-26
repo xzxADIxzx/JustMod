cc55313d18f8a2230e5ca1e35f92b5626154099d
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-01-26 09:22:11 +0300

    ...

--- scripts/GenericSmelter/cotton-wool-mixer.js
+++ scripts/GenericSmelter/cotton-wool-mixer.js
@@ -1,8 +1,12 @@
-var speed = 4.2;
-	const cottonWoolMixer = extendContent(GenericCrafter, "cotton-wool-mixer", {
-		drawLayer(tile){
-		Draw.rect(this.name + "-rotator.png", tile.drawx(), tile.drawy(), tile.entity.totalProgress * speed);
-		Draw.rect(this.name + "-top", tile.drawx(), tile.drawy(), 0);
-		}
-	});
-	cottonWoolMixer.layer = Layer.turret;
+// var speed = 4.2;
+	// const cottonWoolMixer = extendContent(GenericCrafter, "cotton-wool-mixer", {
+		// drawLayer(tile){
+		// Draw.rect(this.name + "-rotator.png", tile.drawx(), tile.drawy(), tile.entity.totalProgress * speed);
+		// Draw.rect(this.name + "-top", tile.drawx(), tile.drawy(), 0);
+		// }
+	// });
+	// cottonWoolMixer.layer = Layer.turret;
+
+const cottonWoolMixer = extendContent (GenericCrafter,"cotton-wool-mixer",{});
+cottonWoolMixer.drawer = new DrawRotator();
+cottonWoolMixer.drawer = new DrawTop();
--- scripts/GenericSmelter/silicone-mixer.js
+++ scripts/GenericSmelter/silicone-mixer.js
@@ -1,8 +1,11 @@
-var speed = -0.5;
-	const siliconeMixer = extendContent(GenericCrafter, "silicone-mixer", {
-		drawLayer(tile){
-		Draw.rect("cotton-wool-mixer-rotator.png", tile.drawx(), tile.drawy(), tile.entity.totalProgress * speed);
-		Draw.rect("cotton-wool-mixer-top", tile.drawx(), tile.drawy(), 0);
-		}
-	});
-	siliconeMixer.layer = Layer.turret;
+// var speed = -0.5;
+	// const siliconeMixer = extendContent(GenericCrafter, "silicone-mixer", {
+		// drawLayer(tile){
+		// Draw.rect("cotton-wool-mixer-rotator.png", tile.drawx(), tile.drawy(), tile.entity.totalProgress * speed);
+		// Draw.rect("cotton-wool-mixer-top", tile.drawx(), tile.drawy(), 0);
+		// }
+	// });
+	// siliconeMixer.layer = Layer.turret;
+
+const siliconeMixer = extendContent (GenericCrafter,"silicone-mixer",{});
+siliconeMixer.drawer = new DrawRotator();
