62ff1667299631cf81933508cdc61853967594d7
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-05-09 21:39:50 +0300

    ...

--- scripts/UnitFactory/air-factory.js
+++ scripts/UnitFactory/air-factory.js
@@ -1,4 +1,4 @@
-this.UnitPlan.UnitPlan(UnitType.payload, 600, null)
+UnitPlan.UnitPlan(UnitType.payload, 600, null)
 print(this)
 print(this.UnitPlan)
-UnitPlan.UnitPlan(UnitType.payload, 600, null)
+this.UnitPlan.UnitPlan(UnitType.payload, 600, null)
